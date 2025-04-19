b04c7eb5c56d72fac4de4c422d398f66e789dd12
Cameron Holland <cameronholland2883@gmail.com>
2025-04-19 16:36:39 -0600

    First version of landing page for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/Logo.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <>
+      <div className="headbar">
+        <div className="logobox">
+          <a href="#">
+            <img src={logo} className="logo" alt="MovieRouterLogo" />
+          </a>
+          <h1 className="caveat">Movie Router</h1>
+        </div>
+        <div className="navbar">
+          <a href="#home">Home</a>
+          <a href="#genres">Genres</a>
+          <a href="#theatres">Theatres</a>
+          <a href="#rec">Recommended</a>
+        </div>
+        <input type="text" placeholder="Search.."/>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to MovieRouter.com</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Welcome welcome
       </p>
     </>
   )
