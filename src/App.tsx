1ea85edb22d277c55f29cd8fbf8071713c01ce61
Cameron Holland <cameronholland2883@gmail.com>
2025-04-19 18:26:42 -0600

    Added google maps embed when you get your location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { useState } from 'react'
 import logo from './assets/Logo.png'
 import './App.css'
+import { API_KEY } from './api-key'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: 100,
+    longitude: 200
+  });
+  const [userLocationError, setUserLocationError] = useState("");
 
+  function getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      setUserLocationError("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  function success(position: GeolocationPosition) {
+    const {latitude, longitude} = position.coords;
+    setUserLocation({ latitude, longitude });
+  }
+  
+  function error() {
+    setUserLocationError("Unable to retrieve your location");
+  }
   return (
       <>
       <div className="headbar">
@@ -24,15 +48,23 @@ function App() {
       </div>
       <h1>Welcome to MovieRouter.com</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => getUserLocation()}>Click to get your location</button>
       </div>
+      {(
+        userLocation.latitude != 100  && <p> You are at {userLocation.latitude}, {userLocation.longitude}</p> &&
+        <iframe
+          width="450"
+          height="250"
+          referrerPolicy="no-referrer-when-downgrade"
+          src={'https://www.google.com/maps/embed/v1/place?key='+API_KEY+'&q='+userLocation.latitude+', '+userLocation.longitude}
+          //</>src={'https://www.google.com/maps/embed/v1/place?key='+API_KEY+'&q=Eiffel+Tower,Paris+France'}>
+        >
+        </iframe>
+      )}
+      {(userLocationError != "" && <p> {userLocationError}</p>)}
       <p>
         Welcome welcome
       </p>
     </>
   )
 }
-
-export default App
