047991eafae15b5f27329a0021d4d7d3261459d1
Cameron Holland <cameronholland2883@gmail.com>
2025-04-21 16:32:08 -0600

    Searchbar is now its own component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,9 @@
 import logo from '../assets/Logo.png'
-import { useState } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './Navbar.css'
+import Searchbar from './Searchbar'
 
 export default function Navbar() {
-    const [query, setQuery] = useState('');
-    const navigate = useNavigate();
-    function queryHandler(){ navigate(`/Search/${query}`)}
-    function changeHandler(e: React.ChangeEvent<HTMLInputElement>){setQuery(e.target.value)}
-    function submitHandler(e: React.FormEvent<HTMLFormElement>){
-        e.preventDefault();
-        queryHandler();
-    }
-
 
     return (
         <>
@@ -31,14 +22,7 @@ export default function Navbar() {
                     <Link to="theatres">Theatres</Link>
                     <Link to="recommended">Recommended</Link>
                 </div>
-                <form className="searchbox" onSubmit={submitHandler}>
-                    <input id='search' type='text' value={query} placeholder='Search' onChange={changeHandler} />
-                    <i onClick={queryHandler}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width = '1.8em' height = '1.8em' viewBox="0 0 512 512">
-                            <path fill='#ffffff' d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
-                        </svg>
-                    </i>
-                </form>
+                <Searchbar />
             </div>
             <main>
                 <Outlet />
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,16 +1,24 @@
-import { useRef } from 'react'
-import { Link, useOutlet, useNavigate } from 'react-router-dom';
-
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Searchbar() {
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate();
+    function queryHandler(){ navigate(`/Search/${query}`)}
+    function changeHandler(e: React.ChangeEvent<HTMLInputElement>){setQuery(e.target.value)}
+    function submitHandler(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        queryHandler();
+    }
+
     return (
-        <>
-            <input type='text' ref={queryRef} placeholder='Search' />
+        <form className="searchbox" onSubmit={submitHandler}>
+        <input id='search' type='text' value={query} placeholder='Search' onChange={changeHandler} />
             <i onClick={queryHandler}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width = '1.8em' height = '1.8em' viewBox="0 0 512 512">
+                    <path fill='#ffffff' d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                 </svg>
             </i>
-        </>
+        </form>
     )
 }
