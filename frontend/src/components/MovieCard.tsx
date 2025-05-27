57f6c304009f7cf050d67947241e289e7097c262
Cameron Holland <cameronholland2883@gmail.com>
2025-05-27 09:28:31 -0600

    update Movie Card

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -20,22 +20,40 @@ type MovieCardProp = {
 
 export default function MovieCard({ movie, text }: MovieCardProp) {
     return (
-    <>
-    <div id="movieCard">
-        <div className="image">
-            <img src={movie.poster} alt={movie.poster} className='poster'></img>
-        </div>
-        <div className={text}>
-            <div className="yearByTitle">
-                <div className="title">{movie.title}</div>
-                <div>({movie.year})</div>
+        <div id="movieCard" className="movie-card">
+            <div className="image">
+                <img src={movie.poster} alt={movie.title} className="poster" />
             </div>
-            <div className="rated">{movie.rated}</div>
-            <div className="released">{movie.released}</div>
-            <div className="genre">{movie.genre}</div>
-            <div className="director">{movie.director}</div>
+            <div className={`${text}`}>
+                <div className="yearByTitle">
+                    <span className="title">{movie.title}</span>
+                    <span className="year">({movie.year})</span>
+                </div>
+                <div className="info-row">
+                    <span className="label">Rated:</span>
+                    <span className="value">{movie.rated}</span>
+                </div>
+                <div className="info-row">
+                    <span className="label">Released:</span>
+                    <span className="value">{movie.released}</span>
+                </div>
+                <div className="info-row">
+                    <span className="label">Genre:</span>
+                    <span className="value">{movie.genre}</span>
+                </div>
+                <div className="info-row">
+                    <span className="label">Director:</span>
+                    <span className="value">{movie.director}</span>
+                </div>
+                <div className="info-row plot-row">
+                    <span className="label">Plot:</span>
+                    <span className="value">{movie.plot}</span>
+                </div>
+                <div className="info-row">
+                    <span className="label">ID:</span>
+                    <span className="value">{movie.id}</span>
+                </div>
+            </div>
         </div>
-    </div>
-    </>
     )
 }
